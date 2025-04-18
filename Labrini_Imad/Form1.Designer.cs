05fb18871f7de14bee31fb3f62bb497d6833b621
Imad Labrini <imadlabrini16@gmail.com>
2025-04-18 16:06:55 +0100

    LoanSimulator

---
+++ Labrini_Imad/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Labrini_Imad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(134, 26);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Tag = "t";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Tag = "c";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(163, 173);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(134, 26);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Tag = "d";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Crédit(DH):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Taux(%):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Durée (année):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(542, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(255, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ajouter votre salaire mensuel (DH):";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(546, 102);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(235, 26);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Tag = "salaire";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(181, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Votre mensualité est de:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.richTextBox1.Location = new System.Drawing.Point(269, 323);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(158, 60);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(111, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 60);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Calculer la mensualité";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.richTextBox2.Location = new System.Drawing.Point(546, 267);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(227, 116);
+            this.richTextBox2.TabIndex = 12;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.Location = new System.Drawing.Point(574, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 53);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "vérifier l\'éligibilité:";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 550);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Labrini_Imad/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labrini_Imad
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        public double calcule_de_mensualite(double c, double t, double a)
+        {
+            double n = a * 12;
+            double tt = t / 100; 
+            double op1 = c * (tt / 12);
+            double op2 = 1 - Math.Pow(1 + (tt / 12), -n);
+            double m = op1 / op2;
+
+            return Math.Round(m, 3);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double credit) &&
+                double.TryParse(textBox2.Text, out double taux) &&
+                double.TryParse(textBox3.Text, out double duree))
+            {
+                double mensualite = calcule_de_mensualite(credit, taux, duree);
+                richTextBox1.Text = mensualite.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Saisissez des valeurs correctes svp!!.");
+            }
+
+        }
+
+        public void eligibility(double salaire, double mensualite)
+        {
+            if (mensualite <= 0.25 * salaire)
+            {
+                richTextBox2.Text = "Vous êtes éligible pour un crédit";
+            
+            }
+            else
+            {
+                richTextBox2.Text = "Vous n'êtes pas éligible pour ce crédit!";
+                String proposition_deal = "\nVous pouvez bénéficier d'un crédit de " + 0.25 * salaire + " DH. " +
+                    "Pour continuer, tapez OK!";
+                richTextBox2.Text += proposition_deal;
+                AjouterBoutonOK();
+            }
+        }
+        private void AjouterBoutonOK()
+        {
+            Button boutonOK = new Button();
+            boutonOK.Text = "OK";
+            boutonOK.Location = new Point(richTextBox2.Location.X, richTextBox2.Location.Y + richTextBox2.Height + 10);
+            boutonOK.Click += new EventHandler(BoutonOK_Click);
+            this.Controls.Add(boutonOK);
+        }
+
+        private void BoutonOK_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Congrats💵, vous etes maintenant éligible pour votre nouveau crédit!" +
+                "Cliquer sur le bouton \'Calculer la mensualité\' pour visualiser votre mensualité." +
+                "\nN.B que vous pouvez changer le taux et la durée comme vous voulez.");
+            double salTag = double.Parse(textBox4.Text);
+            textBox1.Text = (salTag / 4).ToString();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox4.Text, out double salaire))
+            {
+                eligibility(salaire, double.Parse(richTextBox1.Text));
+            }
+            else
+            {
+                MessageBox.Show("Saisissez des valeurs correctes svp!!!.");
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
